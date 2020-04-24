476f23293a37f8b20ba85becf43f4911b7879e1c
Jason <goodcheeseburgers@gmail.com>
2020-04-24 21:23:44 +0100

    first commit

---
+++ cmake-build-debug/CMakeFiles/soil2.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/fason/dev/c_plus_plus/CMake/SOIL2-CMake_fork/src/SOIL2/SOIL2.c" "/home/fason/dev/c_plus_plus/CMake/SOIL2-CMake_fork/cmake-build-debug/CMakeFiles/soil2.dir/src/SOIL2/SOIL2.c.o"
+  "/home/fason/dev/c_plus_plus/CMake/SOIL2-CMake_fork/src/SOIL2/image_DXT.c" "/home/fason/dev/c_plus_plus/CMake/SOIL2-CMake_fork/cmake-build-debug/CMakeFiles/soil2.dir/src/SOIL2/image_DXT.c.o"
+  "/home/fason/dev/c_plus_plus/CMake/SOIL2-CMake_fork/src/SOIL2/image_helper.c" "/home/fason/dev/c_plus_plus/CMake/SOIL2-CMake_fork/cmake-build-debug/CMakeFiles/soil2.dir/src/SOIL2/image_helper.c.o"
+  "/home/fason/dev/c_plus_plus/CMake/SOIL2-CMake_fork/src/SOIL2/wfETC.c" "/home/fason/dev/c_plus_plus/CMake/SOIL2-CMake_fork/cmake-build-debug/CMakeFiles/soil2.dir/src/SOIL2/wfETC.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../src/SOIL2"
+  "../src/common"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/fason/dev/c_plus_plus/CMake/SOIL2-CMake_fork/src/common/common.cpp" "/home/fason/dev/c_plus_plus/CMake/SOIL2-CMake_fork/cmake-build-debug/CMakeFiles/soil2.dir/src/common/common.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src/SOIL2"
+  "../src/common"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
